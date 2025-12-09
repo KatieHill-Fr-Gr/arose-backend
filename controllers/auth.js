5ddeeab33c2b342d81982f2cfb7a50887db3d35f
Katie Hill <khill.fr.gr@gmail.com>
2025-12-09 22:06:40 +0000

    Sign-in route updated with identifiers and to check password using bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,17 +4,41 @@ const router = express.Router()
 import User from '../models/user.js'
 
 
-router.get('/sign-up', (req, res) => {
+router.get('/sign-up', (req, res, next) => {
     console.log(`Request: ${req.method} - ${req.originalUrl}`)
     res.json({ message: 'HIT SIGN UP ROUTE'})
 })
 
-router.get('/sign-in', (req, res) => {
+router.get('/sign-in', (req, res, next) => {
     console.log(`Request: ${req.method} - ${req.originalUrl}`)
+
+    const { identifier, password} = req.body
+
+    try {
+        const foundUser = await User.findOne({
+            $or: [
+                { username: identifier },
+                { email: identifier }
+            ]
+        })
+
+        console.log(foundUser)
+
+        if (!foundUser) throw new Unauthorized('User does not exist')
+        if (!bcrypt.compareSync(password, foundUser.password)) throw new Unauthorized('Password not recognised')
+
+        const token = generateToken(foundUser)    
+        return res.status(201).json({ token: token})
+    } catch (err) {
+        console.log(err)
+        next(err)
+    }
+
+
      res.json({ message: 'HIT SIGN IN ROUTE'})
 })
 
-router.get('/sign-out', (req, res) => {
+router.get('/sign-out', (req, res, next) => {
     console.log(`Request: ${req.method} - ${req.originalUrl}`)
      res.json({ message: 'HIT SIGN OUT ROUTE'})
 })
