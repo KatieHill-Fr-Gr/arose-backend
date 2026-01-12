0e62d6efad366bc0e03cedd9866ea3c00dd04cb4
Katie Hill <khill.fr.gr@gmail.com>
2026-01-12 11:47:38 +0000

    Project model added

---
+++ models/projects.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const projectSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, 'Please enter a project title'],
+        },
+        subtitle: {
+            type: String,
+            required: [true, 'Please enter a one-line summary'],
+        },
+        paragraphs: {
+            type: [String],
+            validate: {
+                validator: function(v) {
+                    return v && v.length >= 1;
+                },
+                message: 'Please add at least one paragraph'
+            }
+        },
+    }
+
+)
+
+const Project = mongoose.model('Project', projectSchema)
+
+export default Project
