d055fe239fe9e659267088d4abc752d24c773dcc
Katie Hill <khill.fr.gr@gmail.com>
2026-01-12 12:51:07 +0000

    Index and Create project routes added

--- controllers/default.js
+++ controllers/default.js
@@ -2,14 +2,11 @@ import express from 'express'
 const router = express.Router()
 
 
-router.get('/', (req, res) => {
-    console.log(`Request: ${req.method} - ${req.originalUrl}`)
-    res.send(`<h1>Welcome to my portfolio</h1>`)
-})
-
-router.get('/about', (req, res) => {
-    console.log(`Request: ${req.method} - ${req.originalUrl}`)
-    res.send(`<h1>About me</h1>`)
+router.get('/', async (req, res, next) => {
+    try {
+    } catch (err) {
+        next(err)
+    }
 })
 
 export { router as defaultRouter }
--- controllers/projects.js
+++ controllers/projects.js
@@ -1,14 +1,34 @@
 import express from 'express'
+
+import Project from '../models/projects.js'
+import errorHandler from '../middleware/errorHandling.js'
+import verifyToken from '../middleware/verifyToken.js' 
+
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    console.log(`Request: ${req.method} - ${req.originalUrl}`)
-    res.send(`<h1>Welcome to my portfolio</h1>`)
+// * Routes
+
+router.get('/', async (req, res, next) => {
+    console.log('Index hit')
+    try {
+        const allProjects = await Project.find()
+        return res.json(allProjects)
+    } catch (err) {
+        console.log(err)
+        next(err)
+    }
 })
 
-router.get('/about', (req, res) => {
-    console.log(`Request: ${req.method} - ${req.originalUrl}`)
-    res.send(`<h1>About me</h1>`)
+router.post('/new', async (req, res, next) => {
+    try {
+        const newProject = await Project.create(req.body)
+        return res.status(201).json(newProject)
+    } catch (err) {
+        next(err)
+    }
 })
+
+
+
 
 export { router as projectsRouter }
