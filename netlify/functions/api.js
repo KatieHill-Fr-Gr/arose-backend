b96a36225d271b7acb0d0c3a26b00ea85b9cfef6
Katie Hill <khill.fr.gr@gmail.com>
2026-01-14 15:36:42 +0000

    Frontend URL added

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(morgan('dev'))
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', process.env.FRONTEND_URL].filter(Boolean),
     credentials: true
 }))
 
